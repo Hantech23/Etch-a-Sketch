8e9b1fa5556ae4c5c7fe618d769fa193618ae8a1
Woobin Han <woobin.han23@gmail.com>
2022-04-08 20:04:37 -0400

    Add hover effect

--- main.js
+++ main.js
@@ -2,9 +2,13 @@
 const grid = document.querySelector('.grid');
 
 for(i = 0; i < 256; i++) {
-    const i = document.createElement('div')
+    const i = document.createElement('grid');
     grid.append(i);
 }
+function changecolor() {
+    this.style.backgroundColor = "black";
+}
 
-
-
+document.querySelectorAll('grid').forEach(item => {
+    item.addEventListener('mouseover', changecolor)
+})
