31f64f8e558f103a91cd66062d2de19ba31306f0
Woobin Han <woobin.han23@gmail.com>
2022-04-09 12:13:03 -0400

    Add reset button

--- main.js
+++ main.js
@@ -5,10 +5,25 @@ for(i = 0; i < 256; i++) {
     const i = document.createElement('grid');
     grid.append(i);
 }
+
+//MOUSEOVER COLORCHANGE
 function changecolor() {
     this.style.backgroundColor = "black";
 }
-
 document.querySelectorAll('grid').forEach(item => {
     item.addEventListener('mouseover', changecolor)
 })
+
+//RESET FUNCTION
+function reset(){
+    document.querySelectorAll('grid').forEach(item => {
+    item.style.backgroundColor = "white";
+    })
+}
+
+
+const body = document.querySelector('body');
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'reset';
+resetBtn.addEventListener('click', reset)
+body.append(resetBtn);
